8a7c28818d021e32e65186b146bd5eed9d420fe9
iamtonmoy0 <tonmoy2code@gmail.com>
2024-03-26 20:13:04 +0600

    Update database connection string and add initial implementation for CRUD operations in service layer
    * Update .env file with new database connection string
    * Update service.go to include CreateStockService function with initial implementation
    * Add GetStockService function with initial implementation
    * Add placeholders for GetAllStockService, UpdateStockService, and DeleteStockService functions with comments only
    
    Note: The commit does not include the implementation for GetAllStockService, UpdateStockService, and DeleteStockService functions.

--- services/service.go
+++ services/service.go
@@ -1,18 +1,58 @@
 package service
 
 import (
+	"database/sql"
+	"fmt"
 	database "go-postgresql-stocks-api/config"
 	"go-postgresql-stocks-api/models"
+	"log"
 )
 
+// create new stock
 func CreateStockService(stock models.Stock) int32 {
 	db := database.CreateConnection()
 	defer db.Close()
+	var id int64
 	sqlQuery := `INSERT INTO stocks(name,price,company) VALUES ($1,$2,$3)`
-	db.QueryRow(sqlQuery,stock.Name,stock.Price,stock.Company)
+	err := db.QueryRow(sqlQuery, stock.Name, stock.Price, stock.Company).Scan(id)
+	if err != nil {
+		log.Fatalf("failed to create stocks %v", err)
+	}
+	fmt.Printf("new stock created")
+	return int32(id)
 
 }
-func GetStockService(id int32) (models.Stock, error)        {}
-func GetAllStockService() ([]models.Stock, error)           {}
-func UpdateStockService(id int32, stock models.Stock) int32 {}
-func DeleteStockService(id int32) int32                     {}
+
+// get stock by id
+func GetStockService(id int32) (models.Stock, error) {
+	db := database.CreateConnection()
+	defer db.Close()
+	var stock models.Stock
+	sqlStatement := `SELECT * FROM stocks WHERE stockid = $1`
+	row := db.QueryRow(sqlStatement, id)
+	err := row.Scan(&stock.StockID, &stock.Name, &stock.Price, &stock.Company)
+
+	switch err {
+	case sql.ErrNoRows:
+		fmt.Println("no rows were returned!")
+		return stock, nil
+	case nil:
+		return stock, nil
+	default:
+		fmt.Println("Unable to scan row %v", err)
+	}
+	return stock, err
+
+}
+func GetAllStockService() ([]models.Stock, error) {
+	db := database.CreateConnection()
+	defer db.Close()
+}
+func UpdateStockService(id int32, stock models.Stock) int32 {
+	db := database.CreateConnection()
+	defer db.Close()
+}
+func DeleteStockService(id int32) int32 {
+	db := database.CreateConnection()
+	defer db.Close()
+}
